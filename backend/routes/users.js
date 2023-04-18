ef9ef44cefcb6968f158c35bb85e01e6603a2cef
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-18 14:21:31 +0300

    remove the user creation handler

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,12 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const {
-  getAllUsers, getUser, createUser, updateAvatar, updateProfile,
+  getAllUsers, getUser, updateAvatar, updateProfile,
 } = require('../controllers/users');
 
 router.get('/', getAllUsers);
 router.get('/:userId', getUser);
-router.post('/', createUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
 

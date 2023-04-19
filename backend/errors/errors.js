e1692e11fb1ffc5c2be97e4a7f36af630b514da2
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-19 14:28:51 +0300

    add const ACCESS_DENIED_ERROR

--- backend/errors/errors.js
+++ backend/errors/errors.js
@@ -1,6 +1,7 @@
 const BAD_REQUEST_STATUS = 400;
 const NOT_FOUND_STATUS = 404;
 const INTERNAL_SERVER_ERROR = 500;
+const ACCESS_DENIED_ERROR = 403;
 const BAD_REQUEST_ERROR_MESSAGE = { message: 'Invalid ID format' };
 const NOT_FOUND_ERR_MESSAGE = { message: 'Requested resource not found' };
 const INTERNAL_SERVER_ERR_MESSAGE = { message: 'An error has occured on the server' };
@@ -13,5 +14,6 @@ module.exports = {
   BAD_REQUEST_ERROR_MESSAGE,
   NOT_FOUND_ERR_MESSAGE,
   INTERNAL_SERVER_ERR_MESSAGE,
-  UNAUTHORIZED_ERR_MESSAGE
+  UNAUTHORIZED_ERR_MESSAGE,
+  ACCESS_DENIED_ERROR
 };

3947c531e83463d01d156de22b0f90f9693ca772
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-04-30 17:34:44 +0300

    add code for server crash testing

--- backend/app.js
+++ backend/app.js
@@ -41,7 +41,17 @@ mongoose.connect('mongodb://localhost:27017/aroundb');
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 app.post('/signin', validateAuthentication, login);
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 app.post('/signup', validateUserBody, createUser);
 app.use(auth);
 app.use('/users', userRouter);

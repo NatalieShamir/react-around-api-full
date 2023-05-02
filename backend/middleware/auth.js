e55e7ae865547db1580bf074de1af6bfece89f43
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-02 13:06:59 +0300

    pass 401 code error class to next

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/config');
-const { UNAUTHORIZED_ERR_MESSAGE } = require('../errors/errors');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(UNAUTHORIZED_ERR_MESSAGE);
+    return next(UnauthorizedError);
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,7 +16,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return next(UNAUTHORIZED_ERR_MESSAGE);
+    return next(UnauthorizedError);
   }
 
   req.user = payload;

baf4dd8fc0ec4c59f8df86a44872c58ef759659e
Natalie Shamir <45148439+NatalieShamir@users.noreply.github.com>
2023-05-21 14:58:46 +0300

    change routes

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -10,9 +10,9 @@ const { validateCardBody, validateCardId } = require('../middleware/validation')
 
 router.get('/', getAllCards);
 router.post('/', validateCardBody, createCard);
-router.delete('/cards/:cardId', validateCardId, deleteCard);
-router.put('/cards/likes/:cardId', validateCardId, likeCard);
-router.delete('/cards/likes/:cardId', validateCardId, dislikeCard);
+router.delete('/:cardId', validateCardId, deleteCard);
+router.put('/likes/:cardId', validateCardId, likeCard);
+router.delete('/likes/:cardId', validateCardId, dislikeCard);
 
 module.exports = {
   cardRouter: router,
